f386e9e37d074f1672705c5eb3dfe433bae9a7bb
りんご飴196 <kazumac41@gmail.com>
2025-03-05 22:57:21 +0900

    ✨  :config生成するように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,6 +1,7 @@
 package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
+import com.github.ringoame196_s_mcPlugin.datas.Data
 import com.github.ringoame196_s_mcPlugin.events.ItemFrameEvents
 import com.github.ringoame196_s_mcPlugin.managers.ImgMapManager
 import org.bukkit.plugin.java.JavaPlugin
@@ -11,6 +12,8 @@ class Main : JavaPlugin() {
 
     override fun onEnable() {
         super.onEnable()
+        saveDefaultConfig() // config生成
+        Data.max = config.getInt("max")
         server.pluginManager.registerEvents(ItemFrameEvents(plugin), plugin)
         val command = getCommand("imagepuzzle")
         command!!.setExecutor(Command())
